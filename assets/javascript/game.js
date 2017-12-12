8e8a104d6aaf203c8e133d411b013b2a0fade7bd
Eric Traband <ejstraband@gmail.com>
2017-12-12 14:50:48 -0600

    variables and functions defined

---
+++ assets/javascript/game.js
@@ -0,0 +1,39 @@
+// global variables
+var wins = 0;
+var losses = 0;
+var targetScore = 0;
+var myScore = 0;
+var crystalValues = [0, 0, 0, 0];
+
+console.log("current values: " + wins, losses, targetScore, myScore, crystalValues);
+
+// primary functions
+function gameStart() {
+	generateTargetScore();
+	generateCrystalValues();
+}
+
+function generateTargetScore() {
+
+}
+
+function generateCrystalValues() {
+
+}
+
+function win() {
+	wins = (wins+1);
+	gameStart();
+}
+
+function lose() {
+	losses = (losses+1);
+	gameStart();	
+}
+
+// main app logic
+
+$("crystal").on("click", function() {
+
+
+});
